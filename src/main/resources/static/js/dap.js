a7b7cc3952cda5ab7e0864d66e71af27f5f4476f
house <house@DESKTOP-HOKKH3G>
2019-04-08 16:47:04 +0200

    HTML & JS update

--- src/main/resources/static/js/dap.js
+++ src/main/resources/static/js/dap.js
@@ -6,3 +6,9 @@ myJsFunc2 = function() {
 	event=prompt("Utilisateur ?");
 	window.open("http://localhost:8080/event/next?userKey=" + event);
 }
+myJsFunc3 = function() {
+	user=prompt("Utilisateur ?");
+	newuser=prompt("Nouvel Utilisateur ?")
+	newuser=newuser.replace( /[^a-zA-Z]/ , "");
+	window.open("http://localhost:8080/changeUserName?userKey="+ user +"&newUserKey=" + newuser);
+}
