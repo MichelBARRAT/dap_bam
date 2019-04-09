55051ae0a91139273cfd0ad0bfd3d9794d160c0b
house <house@DESKTOP-HOKKH3G>
2019-04-09 16:26:43 +0200

    update Front

--- src/main/resources/static/js/dap.js
+++ src/main/resources/static/js/dap.js
@@ -1,14 +1,14 @@
-myJsFunc = function() {
+userGmail = function() {
 	email=prompt("Utilisateur ?");
 	window.open("http://localhost:8080/email/nbunread?userKey=" + email);
 }
-myJsFunc2 = function() {
+userEvent = function() {
 	event=prompt("Utilisateur ?");
 	window.open("http://localhost:8080/event/next?userKey=" + event);
 }
-myJsFunc3 = function() {
+userRename = function() {
 	user=prompt("Utilisateur ?");
 	newuser=prompt("Nouvel Utilisateur ?")
 	newuser=newuser.replace( /[^a-zA-Z]/ , "");
-	window.open("http://localhost:8080/changeUserName?userKey="+ user +"&newUserKey=" + newuser);
+	window.open("http://localhost:8080/changeUserName?userKey="+ user +"&newUserKey=" + newuser, "_self" );
 }
